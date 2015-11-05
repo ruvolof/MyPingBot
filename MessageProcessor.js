62c1a52037db0ccc7855b0efd2ca3a397fa202e5
Francesco Ruvolo <ruvolof@gmail.com>
2015-11-05 14:04:07 +0100

    Added test for maintenance mode.

--- MessageProcessor.js
+++ MessageProcessor.js
@@ -1,9 +1,18 @@
 var nba = require('./NodeBotAPI.js');
 var monitor = require('./Monitor.js');
 var ping = require('./node_modules/ping');
+var config = require('./config');
 
 exports.processMessage = function (update_id, msg) {
   console.log('Processing message '+update_id+', message id '+msg.message_id+ ', from '+msg.from.username+' '+msg.from.id);
+
+  if (config.maintenance) {
+    if (config.testers.indexOf(msg.from.id) == -1) {
+      var s = 'Currently under maintenance. Bot will reply only to developers and testers.';
+      nba.sendMessage(msg.from.id, s.toString('utf8'));
+      return;
+    }
+  }
 
   if (/^\/ping/.test(msg.text)) {
     var re_args = /^\/ping\s+([\.:\/a-z0-9]+)$/g;
